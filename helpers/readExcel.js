323fd7499b66bb45318f9a7ca45fc02b9a39eda1
unknown <mostafaswifyy@gmail.com>
2024-04-13 02:35:52 +0800

    update code

--- helpers/readExcel.js
+++ helpers/readExcel.js
@@ -1,14 +1,7 @@
 const XLSX = require("xlsx");
 module.exports = readExcelData = (filePath) => {
-  // read the excel file
   const workbook = XLSX.readFile(filePath);
-
-  // select the first sheet
   const sheet = workbook.Sheets[workbook.SheetNames[0]];
-
-  // convert the sheet to JSON format
   const data = XLSX.utils.sheet_to_json(sheet);
-
-  // return the data as an array
   return data;
 };
